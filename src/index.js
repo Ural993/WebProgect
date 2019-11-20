6afe7b6e9df3b7f5255a06711d3c2c7048182c99
Ural Islamov <ural-islamov@mail.ru>
2019-11-20 23:37:34 +0500

    downloaded have been set webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import "./js/common";
-import "./css/main.css";
-import "./scss/main.scss";
+// JS
+import './js/'
+
+// SCSS
+import './assets/scss/main.scss'
+
+// CSS (example)
+// import './assets/css/main.css'
+
+

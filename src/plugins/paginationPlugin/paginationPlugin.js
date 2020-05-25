df210f95b1ac9166fa65ea0426584b0cf6ba073d
ural993 <ural-islamov@mail.ru>
2020-05-25 17:47:10 +0300

    doing pagination

--- src/plugins/paginationPlugin/paginationPlugin.js
+++ src/plugins/paginationPlugin/paginationPlugin.js
@@ -9,18 +9,17 @@
         const $border = $('.paginationBlock');
         // const $counter = $(`<span>${itemCount}</span>`).addClass(countCls);
         // $('.likeButton__block').append($counter);
-        let totalItemsCount = 15;
-        let pageSize = 5;
-        let pagesCount = Math.ceil(totalItemsCount / pageSize);
+        let totalPages = 15;
         let pages = [];
-        for (let i = 1; i <= totalItemsCount; i++) {
+        for (let i = 1; i <= totalPages; i++) {
             pages.push(i);}
-    
-        //let portionCount = Math.ceil(pagesCount / portionSize);
-        //let leftPortionPageNumber = (portionNumber-1)* portionSize + 1;
-        //let rightPortionPageNumber = portionNumber* portionSize;
+        let portionSize = 3;
+        let portionNumber = 1;
+        let portionCount = Math.ceil(totalPages / portionSize);
+        let leftPortionPageNumber = (portionNumber-1)* portionSize + 1;
+        let rightPortionPageNumber = portionNumber* portionSize;
         let paginator = pages
-        //.filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+        .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
         .map(p => {
         return $(`<span ${onClick=> onPageChanged(p)}>${p}</span>`)
     })
